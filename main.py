edeee20ac008d9b6edc196741031fd34a05cc668
Yuna Bear <75515824+yunabear@users.noreply.github.com>
2021-01-03 16:06:41 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.content.find("$hello") != -1:
+      await message.channel.send("Hi")
+
+    if message.content.find("$instagram") != -1:
+      await message.channel.send("https://www.instagram.com/bearyuna_/")
+
+    if message.content.find("$youtube") != -1:
+      await message.channel.send("https://www.youtube.com/channel/UC7Tn2xZSgIzojYpH23dSmAQ")
+
+    if message.content.find("$author") != -1:
+      await message.channel.send("By Yuna Bear")
+
+client.run(os.getenv('TOKEN'))
